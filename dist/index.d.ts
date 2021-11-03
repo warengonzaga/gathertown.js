4d666b07810c9f3aca79c3d57fc2f7b51e985b1a
princejoogie <princejoogie@gmail.com>
2021-11-04 05:27:37 +0800

    removed dist from repo

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-interface IGather {
-  getMap(spaceId: string, mapId: string): Promise<any>;
-  getEmailGuestlist(spaceId: string): Promise<any>;
-}
-export default function Gather(initialApiKey: string): IGather;
-export const useGather: (apiKey: string) => IGather;
-export default Gather;
-
-//# sourceMappingURL=index.d.ts.map
