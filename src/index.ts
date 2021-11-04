f802537bbcad4097e6502283760531ab9603ada9
Prince Carlo Juguilon <47204120+princejoogie@users.noreply.github.com>
2021-11-04 16:47:53 +0800

    ✨ TWEAK: converted to typescript (#24)
    * added eslint and husky
    
    * converted js files to ts
    
    * added dist to eslint ignore
    
    * added prettier and eslint config files
    
    * added `useGather` hook
    
    * removed dist from repo
    
    * included dist
    
    * built the project
    
    * single quote and remove trailing commas config for prettier
    
    * added some rules to `.eslintrc.json`
    
    * added back trailing commas
    
    * added `.editorconfig` file

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import requester from './requester';
+
+interface IGather {
+  getMap(spaceId: string, mapId: string): Promise<any>;
+  getEmailGuestlist(spaceId: string): Promise<any>;
+}
+
+function Gather(initialApiKey: string): IGather {
+  const getMap = (spaceId: string, mapId: string) => {
+    // TODO: Catch nullish value for params
+    const formattedSpaceID = spaceId.replace(/\//gi, '\\\\');
+    const space = '?spaceId=' + formattedSpaceID;
+    const map = '&mapId=' + mapId;
+    const apiKey = '&apiKey=' + initialApiKey;
+    return _sendRequest('getMap' + space + map + apiKey);
+  };
+
+  const getEmailGuestlist = (spaceId: string) => {
+    // TODO: Catch nullish value for params
+    const space = '?spaceId=' + spaceId;
+    const apiKey = '&apiKey=' + initialApiKey;
+    return _sendRequest('getEmailGuestlist' + space + apiKey);
+  };
+
+  const _sendRequest = async (path: string) => {
+    try {
+      const res = await requester.get(path, {
+        validateStatus: function (status) {
+          return status >= 200 && status < 300;
+        },
+      });
+      return res.data;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  return {
+    getEmailGuestlist,
+    getMap,
+  };
+}
+
+export const useGather = (apiKey: string) => {
+  return Gather(apiKey);
+};
+
+export default Gather;
---
+++ src/requester.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import config from './config';
+const { baseURL } = config;
+
+const requester = axios.create({
+  baseURL,
+  responseType: 'json',
+  validateStatus: (status) => status === 200,
+});
+
+export default requester;
